a49aa796baccb36d7eec1a141eab49ecfa01669e
Soren Macbeth <soren@dopeness.org>
2014-06-08 01:11:05 -0700

    update deps

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject lein-scrooge "0.1.3-SNAPSHOT"
+(defproject org.clojars.sorenmacbeth/lein-scrooge "0.1.3-SNAPSHOT"
   :description "Leiningen plugin to run Twitter Scrooge compiler on Thrift IDL files"
   :url "http://github.com/jcrean/lein-scrooge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dev-dependencies [[swank-clojure "1.4.2"]]
-  :dependencies [[com.twitter/finagle-core "6.3.0"]
-                 [com.twitter/finagle-thrift "6.3.0"]
-                 [com.twitter/scrooge-runtime "3.1.1"]
-                 [com.twitter/scrooge-generator "3.1.1"]
-                 [org.apache.thrift/libthrift "0.8.0"]]
+  :dependencies [[com.twitter/finagle-core_2.10 "6.16.0"]
+                 [com.twitter/finagle-thrift_2.10 "6.16.0"]
+                 [com.twitter/scrooge-runtime_2.10 "3.15.0"]
+                 [com.twitter/scrooge-generator_2.10 "3.15.0"]
+                 [org.apache.thrift/libthrift "0.9.1"]]
   :eval-in-leiningen true)
